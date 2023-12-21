8bd686d6fa92354539e2b723a705bb4e4639f2ac
Brody <brody2.wilton@live.uwe.ac.uk>
2023-12-21 21:38:31 +0000

    Implement psuedocode

--- task_one_pt2.cpp
+++ task_one_pt2.cpp
@@ -1,7 +1,14 @@
+#include <iostream>
+#include <cstdint>
+#include "context/context.hpp"
+
+
 // func foo:
 int foo(){
-    // output "you called foo"
-    // call function exit
+    //output 'you called foo'
+    std::cout << "you called foo" << std::endl;
+    //call function exit
+    exit(0);
     return 0;
 }
 
@@ -9,15 +16,26 @@ int foo(){
 int main(){
     // allocate space for stack
     // data is an array of 4096 characters
-      
+    char stack[4096];
     // stack grows downards
     // sp is a pointer to characters
     // set sp to data plus 4096
+    char *sp = stack + 4096;
+
+    //align to 16 bytes and correctly convert between ptr types
+    sp = reinterpret_cast<char*>(reinterpret_cast<uintptr_t>(sp) & -16L);
+
+    //adjust for red zone
+    sp -= 128;
     
     // create an empty context c
+    Context c;
     // set rip of c to foo
+    c.rip = reinterpret_cast<void*>(&foo);
     // set rsp of c to sp
-
+    c.rsp = sp;
     // call set_context with c 
+    set_context(&c);
+
     return 0;
 }
