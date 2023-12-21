39dcf333aea0196b4bec487e7d6ee1a2caca0aca
Brody <brody2.wilton@live.uwe.ac.uk>
2023-12-21 17:26:54 +0000

    Implement psuedocode for task one part one

--- task_one.cpp
+++ task_one.cpp
@@ -3,12 +3,20 @@
 #include "context/context.hpp"
 
 int main(){
-// 1. set x to 0 (volatile)
-// 2. set c to get_context
-// 3. output "a message"
-// 4. if x == 0:
-// 5.   set x to x plus one
-// 6.   call set_context with c
-
+    // 1. set x to 0 (volatile)
+    volatile int x = 0;
+    // 2. set c to get_context
+    Context c;
+    get_context(&c);
+    // 3. output "a message"
+    std::cout << "a message" << std::endl;
+    // 4. if x == 0:
+    if (x == 0) {
+        // 5. set x to x plus one
+        x = x + 1;
+        // 6. call set_context with c
+        set_context(&c);
+    }
+    
     return 0;
 }
