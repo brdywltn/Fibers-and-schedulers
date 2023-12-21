fa252aedf1b37523246b06db578c2b259d23fefc
Brody <brody2.wilton@live.uwe.ac.uk>
2023-12-21 18:18:42 +0000

    Add task one pt 2 file and psuedocode

---
+++ task_one_pt2.cpp
@@ -0,0 +1,17 @@
+// func foo:
+//      output "you called foo"
+//      call function exit
+
+//func main:
+//      allocate space for stack
+//      data is an array of 4096 characters
+//      
+//      stack grows downards
+//      sp is a pointer to characters
+//      set sp to data plus 4096
+//      
+//      create an empty context c
+//      set rip of c to foo
+//      set rsp of c to sp
+//
+//      call set_context with c      
