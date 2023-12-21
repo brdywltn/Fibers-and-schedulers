c716916157cdcc8f2ee2843c1a6f0960e4ee8d7f
Brody <brody2.wilton@live.uwe.ac.uk>
2023-12-21 17:22:36 +0000

    add includes and main function body wrapper

---
+++ task_one.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cstdint>
+#include "context/context.hpp"
+
+int main(){
+// 1. set x to 0 (volatile)
+// 2. set c to get_context
+// 3. output "a message"
+// 4. if x == 0:
+// 5.   set x to x plus one
+// 6.   call set_context with c
+
+    return 0;
+}
