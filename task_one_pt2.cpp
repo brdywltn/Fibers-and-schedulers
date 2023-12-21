a5ab5f77cdac66da299f56093742636ecc99560a
Brody <brody2.wilton@live.uwe.ac.uk>
2023-12-21 18:34:42 +0000

    Add psuedocode, main function and foo function skeletons

--- task_one_pt2.cpp
+++ task_one_pt2.cpp
@@ -1,17 +1,23 @@
 // func foo:
-//      output "you called foo"
-//      call function exit
+int foo(){
+    // output "you called foo"
+    // call function exit
+    return 0;
+}
 
 //func main:
-//      allocate space for stack
-//      data is an array of 4096 characters
-//      
-//      stack grows downards
-//      sp is a pointer to characters
-//      set sp to data plus 4096
-//      
-//      create an empty context c
-//      set rip of c to foo
-//      set rsp of c to sp
-//
-//      call set_context with c      
+int main(){
+    // allocate space for stack
+    // data is an array of 4096 characters
+      
+    // stack grows downards
+    // sp is a pointer to characters
+    // set sp to data plus 4096
+    
+    // create an empty context c
+    // set rip of c to foo
+    // set rsp of c to sp
+
+    // call set_context with c 
+    return 0;
+}
