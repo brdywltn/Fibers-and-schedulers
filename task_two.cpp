72b2881a5ab7250999c61bfb928482819b3fbe92
Brody <brody2.wilton@live.uwe.ac.uk>
2023-12-22 18:50:08 +0000

    add fiber class skeleton and psuedocode

--- task_two.cpp
+++ task_two.cpp
@@ -3,10 +3,26 @@
 #include "context/context.hpp"
 
 // fiber class
+class Fiber{
+private:
+    //context
+    //stack bottom
+    //stack top
+public:
+    //fiber constructor
+    //fiber destructor
+    //get context
+};
 
 // foo func
+void foo (){
+    std::cout << "Foo fighter" << std::endl;
+}
 
 // main
+int main(){
     //set f by creating fiber with foo
     //set c calling method function get_context from f
     //call set_context with c
+    return 0;
+}
