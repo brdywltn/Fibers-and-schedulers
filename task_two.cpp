11992a75bdad7d7a55fee02ac9597134ab7098fc
Brody <brody2.wilton@live.uwe.ac.uk>
2023-12-22 18:47:49 +0000

    initial commit with psuedocode

---
+++ task_two.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cstdint>
+#include "context/context.hpp"
+
+// fiber class
+
+// foo func
+
+// main
+    //set f by creating fiber with foo
+    //set c calling method function get_context from f
+    //call set_context with c
