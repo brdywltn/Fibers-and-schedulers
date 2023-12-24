68f16b280b5d7e30cdd95c157f3789ab46a53a79
Brody <brody2.wilton@live.uwe.ac.uk>
2023-12-24 10:52:43 +0000

    implement scheduler

--- task_two.cpp
+++ task_two.cpp
@@ -1,28 +1,8 @@
 #include <iostream>
 #include <cstdint>
+#include <deque>
 #include "context/context.hpp"
 
-class Scheduler {
-private:
-//fibers_:std::deque<fiber*>
-//context_: Context
-public:
-//scheduler()
-    //initialise context
-//~scheduler
-    //get rid
-//spawn(f:fiber*): void
-    //add context to back of queue
-//do_it(): void
-    //return here to reenter scheduler
-    //continue executing fibers in RR mode
-        //pop front fiber from queue
-        //set entry point for fiber
-//fiber_exit(): void
-    //jump back to loop
-};
-
-//global scheduler instance?
 
 // fiber class
 class Fiber {
@@ -33,11 +13,13 @@ private:
     char* stack_bottom_;
     //stack top
     char* stack_top_;
+    //char* stack;
 public:
     //fiber constructor
     Fiber(void (*function)()){
         //allocate space for stack
         char stack[4096];
+        //stack = new char[4096];
         //stack grows down
         char* sp = stack + 4096;
 
@@ -61,8 +43,7 @@ public:
 
     //fiber destructor
     ~Fiber() {
-        delete stack_bottom_;
-        delete stack_top_;
+        //delete[] stack;
     }
 
     //get context
@@ -71,20 +52,68 @@ public:
     }
 };
 
+class Scheduler {
+private:
+    //fibers_:std::deque<fiber*>
+    std::deque<Fiber*> fibers_;
+    //context_: Context
+    Context context_;
+public:
+    //scheduler()
+    Scheduler(){
+        //initialise context
+    }
+    //~scheduler
+    ~Scheduler(){
+        //get rid
+    }
+    //spawn(f:fiber*): void
+    void spawn(Fiber* f) {
+        //add context to back of queue
+        fibers_.push_back(f);
+    }
+
+    //do_it(): void
+    void do_it(){
+        //return here to reenter scheduler
+        get_context(&context_);
+        //continue executing fibers in RR mode
+        while (!fibers_.empty()) {
+            //pop front fiber from queue
+            Fiber* f = fibers_.front();
+            fibers_.pop_front();
+            //set entry point for fiber
+            Context c = f->get_context();
+            set_context(&c);
+        }
+    }
+    //fiber_exit(): void
+    void fiber_exit() {
+        //jump back to loop
+        set_context(&context_);
+    }
+};
+
+//global scheduler instance?
+Scheduler s;
+
 // foo func
 void foo (){
     std::cout << "Foo fighter" << std::endl;
-    exit(0);
+    s.fiber_exit();
 }
 
 // main
 int main(){
     //set f by creating fiber with foo
-    Fiber f(&foo);
+    Fiber f1(&foo);
+    Fiber f2(&foo);
+    Fiber f3(&foo);
     //set c calling method function get_context from f
-    Context c = f.get_context();
-    //call set_context with c
-    set_context(&c);
+    s.spawn(&f1);
+    s.spawn(&f2);
+    s.spawn(&f3);
+    s.do_it();
     
     return 0;
 }
